a161df19c6c3287503c16f3fbfed3e5f188babff
Ayush Singh <asayushsingh80@gmail.com>
2024-04-19 22:48:30 +0530

    ui: light mode colors for drop indicators

--- src/components/DropListIndicator.tsx
+++ src/components/DropListIndicator.tsx
@@ -3,7 +3,7 @@ export default function DropListIndicator({ beforeId }: { beforeId: string }) {
     <div
       data-beforelist={beforeId}
       data-src={"#list"}
-      className="bg-white opacity-0 w-0.5 mx-0.5 rounded"
+      className="bg-black dark:bg-white opacity-0 w-0.5 mx-0.5 rounded"
     />
   );
 }
--- src/components/DropTaskIndicator.tsx
+++ src/components/DropTaskIndicator.tsx
@@ -9,7 +9,7 @@ export default function DropTaskIndicator({
     <div
       data-before={beforeId || "-1"}
       data-list={listId}
-      className="my-0.5 h-0.5 w-full bg-white opacity-0 rounded"
+      className="my-0.5 h-0.5 w-full bg-black dark:bg-white opacity-0 rounded"
     />
   );
 }
